a82ad103327e90d68e17dce05dd0342bf3193066
BJC80_1555 <66450547+BIKOUMBOU@users.noreply.github.com>
2023-05-05 15:26:05 +0200

    Add files via upload

---
+++ imedsProject/src/main/java/com/isisptut/imedsProject/ImedsProjectApplication.java
@@ -0,0 +1,13 @@
+package com.isisptut.imedsProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImedsProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImedsProjectApplication.class, args);
+	}
+
+}
---
+++ imedsProject/src/test/java/com/isisptut/imedsProject/ImedsProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.isisptut.imedsProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImedsProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
